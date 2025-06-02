de9a887ab61412636905a2004d64e8f6bb9150e1
devvspl <devendrakumar.vspl@gmail.com>
2025-06-02 17:36:18 +0530

    Update

--- application/controllers/Record.php
+++ application/controllers/Record.php
@@ -55,9 +55,9 @@ class Record extends CI_Controller {
 	public function vspl_index($scan_id, $DocTypeId) {
 		$this->session->set_userdata('top_menu', 'punch_master');
 		$this->session->set_userdata('sub_menu', 'punch');
-		$this->data['file_detail'] = $this->Record_model->vspl_getRecordFile_Accounting($scan_id);
-		$mainRecord = $this->get_additional_information_by_scan_id($scan_id);
-		$this->data['main_record'] = $mainRecord ? $mainRecord : null;
+		// $this->data['file_detail'] = $this->Record_model->vspl_getRecordFile_Accounting($scan_id);
+		// $mainRecord = $this->get_additional_information_by_scan_id($scan_id);
+		// $this->data['main_record'] = $mainRecord ? $mainRecord : null;
 		$this->data['main'] = 'records/_vspl_record';
 		$this->load->view('layout/template', $this->data);
 	}
